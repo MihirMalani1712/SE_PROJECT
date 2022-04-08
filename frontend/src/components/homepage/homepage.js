9b7c79cd5602e5088f5fc5313cc525b3ab5a16aa
MihirMalani1712 <69343657+MihirMalani1712@users.noreply.github.com>
2022-04-08 08:47:16 +0530

    Add files via upload

---
+++ frontend/src/components/homepage/homepage.js
@@ -0,0 +1,41 @@
+import React from "react"
+import "./homepage.css"
+
+
+const Homepage = ({setLoginUser}) => {
+    const data = [
+        { Title: "Financial Markets-Senior Analyst-KYC", Company: 'eClerx', Rating: "3.6" },
+        { Title: "Senior Data Analyst - Workforce Management Planning", Company: 'idfc first bank', Rating: "3.9" },
+        { Title: "Data Analyst", Company: 'Bauer Corporate Services Pvt Ltd', Rating: "3.6" },
+        { Title: "Data Quality Business Analyst", Company: 'BNP Paribas', Rating: "3.9" },
+        { Title: "MIS/ Data Analyst", Company: 'Reach Local Services Pvt Ltd.', Rating: "3.1" }
+      ]
+      
+    return (
+        <div className="homepage">
+            <h1  class="order-header">All-at-One Job Portal</h1>
+            <br></br>
+            <h2  class="order-header">Job openings</h2>
+            <table>
+
+                <tr>
+                    <th>Title</th>
+                    <th>Company</th>
+                    <th>Rating</th>
+                </tr>
+                {data.map((val, key) => {
+                return (
+                    <tr key={key}>
+                        <td>{val.Title}</td>
+                        <td>{val.Company}</td>
+                        <td>{val.Rating}</td>
+                    </tr>
+                )
+                })}
+            </table>
+            <div className="button" onClick={() => setLoginUser({})} >Logout</div>
+        </div>
+    )
+}
+
+export default Homepage
